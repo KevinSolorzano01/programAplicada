c97ae1fa1e9da489a426e4f5c4565fc6ac5d4426
KevinSolorzano01 <142328153+KevinSolorzano01@users.noreply.github.com>
2023-09-10 13:46:20 -0500

    Create homework.py

---
+++ Bucles/homework.py
@@ -0,0 +1,22 @@
+""" Toma dos numeros y define su tipo de dato, uno lo pone entero y el otro float, los suma y muestra el resultado """
+
+
+a = input("Enter a number: ")
+a = int(a)
+b = input("Enter b number: ")
+b = float(b)
+c = a + b
+
+if a == b:
+    print("equal")
+else:
+    print("Different")
+
+print("Type of a is: ", type(a))
+print("Type of b is: ", type(b))
+print("c = ", c)
+
+if type(a) == type(b):
+    print("a and b are of the same type")
+else:
+    print("a and b are of different type")
