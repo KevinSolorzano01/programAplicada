2e0d7f61d62fe1bf2b9b6f4671a1c66f6bc3b36f
KevinSolorzano01 <142328153+KevinSolorzano01@users.noreply.github.com>
2023-09-10 13:22:37 -0500

    Create factorial.py

---
+++ Bucles/factorial.py
@@ -0,0 +1,14 @@
+"""Es un bucle infinito while """
+
+while True:
+    valor =(input("introducion un numero positivo: "))
+    print("Valor: ", valor)
+    a = isinstance(valor,int)
+    print(a)
+    if a== True and valor > 0:
+        fact=1
+        for i in range(1, valor+1):
+            fact=fact*i
+            print(f'the factorial de {valor} es: ', fact)
+    else:
+        print("por favor, ingresar un numero positivo")
