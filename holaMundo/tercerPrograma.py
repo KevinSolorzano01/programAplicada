4b875651ec9d50968c6dd8101ec37e3dd03568cf
KevinSolorzano01 <142328153+KevinSolorzano01@users.noreply.github.com>
2023-09-10 13:13:12 -0500

    Create tercerPrograma.py

---
+++ holaMundo/tercerPrograma.py
@@ -0,0 +1,24 @@
+# Este codigo muestra como actualizar variables.
+
+
+# ¡Hemos definido aquí la variable "comida" con el nombre de la comida que comimos en el desayuno!
+comida = "Un panecillo inglés"
+meal = "An english muffin"
+
+# Imprimir el desayuno
+print("Breakfast:")
+print(meal)
+
+# ¡Ahora actualice la comida para que sea el almuerzo!
+comida = "Una receta colombiana"
+meal = "A colombian recipe"
+
+# Imprimir el almuerzo
+print("Lunch:")
+print(meal)
+
+# Ahora actualiza "comida" para que sea cena
+
+# Printing out dinner
+print("Dinner:")
+print(meal)
