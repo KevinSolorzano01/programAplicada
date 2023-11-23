1bdce3b3c820b3bb7ae62c5f47a3abfac805c9cf
‮KevinSolorzano01 <142328153+KevinSolorzano01@users.noreply.github.com>
2023-11-23 12:10:19 -0500

    Update TXT.py

--- SegundoCorte/ManejoDeArchivos/TXT.py
+++ SegundoCorte/ManejoDeArchivos/TXT.py
@@ -1,1 +1,24 @@
+import random
+import csv
+import os
 
+
+try:
+    os.remove("pseudonumero.csv")
+except OSError:
+    print("No se pudo borrar el archivo")
+
+
+n = int(input("Ingrese la cantidad de numero que desea guardar: "))
+
+list = []
+for i in range (n):
+        num = random.uniform(0,1)
+        numr = round(num, 2)
+        list.append(numr)
+    
+
+with open('pseudonumero.csv', 'a') as a:
+    numero = csv.writer(a)
+    numero.writerow(list)
+    a.close()
